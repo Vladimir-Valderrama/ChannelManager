5904ec42d408db29b6c4f8222c0515562f9c8f46
GatitoConBotas <pipobray@gmail.com>
2024-10-08 15:46:54 -0300

    database

--- services/transaccion-service/index.js
+++ services/transaccion-service/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 4003;
 
-app.get('/', (req, res) => res.send('Informe Service'));
-app.listen(port, () => console.log(`Informe Service running on port ${port}`));
+app.get('/prueba', (req, res) => res.send('Informe Service'));
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));

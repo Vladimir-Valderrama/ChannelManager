56e0cddb36b38ea8c8a20da6956e17871b940ca2
GatitoConBotas <pipobray@gmail.com>
2024-10-08 16:18:05 -0300

    database2

--- services/transaccion-service/index.js
+++ services/transaccion-service/index.js
@@ -2,5 +2,23 @@ const express = require('express');
 const app = express();
 const port = 4003;
 
-app.get('/prueba', (req, res) => res.send('Informe Service'));
+//conexion a mongodb
+const mongoose = require('mongoose');
+
+// URL de conexión (para localhost)
+const uri = "mongodb://localhost:27017/db-piola";
+
+// Conectar a MongoDB con Mongoose
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB');
+  })
+  .catch(err => {
+    console.error('Error al conectar con MongoDB:', err);
+  });
+
+
+
+app.get('/get', (req, res) => res.send('Informe Service'));
 app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
+

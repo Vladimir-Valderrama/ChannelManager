a54d6632944e5f0bc6b2f3e1441caddcc04b5731
Mauri <mauricio.caneo@genstudents.org>
2024-10-16 12:54:03 -0300

    reservas microservicio ok

--- services/reserva-service/index.js
+++ services/reserva-service/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const port = 4002;
 
-app.get('/', (req, res) => res.send('Informe Service'));
-app.listen(port, () => console.log(`Informe Service running on port ${port}`));
+// Middleware para el manejo de json.
+app.use(express.json());
+
+// Ruta del archivo json (base de datos).
+const dbPath = path.join(__dirname, '..', '..', 'backend', 'database-prueba.json');
+
+// Funcion para leer la base de datos de json.
+const readData = () => {
+    const data = fs.readFileSync(dbPath);
+    return JSON.parse(data);
+}
+
+// Endpoint para obtener todos las reservas.
+app.get('/reservas', (req, res) => {
+    const data = readData();
+    res.json(data.RESERVATIONS);
+});
+
+// Ruta del microservicio.
+app.listen(port, () => console.log(`Informe Service corriendo en http://localhost:${port}`));
